004440612903ba4842c79b6c02da5e54616e9359
Alexander Levitsky <alexander.v.levitsky@gmail.com>
2019-04-25 23:54:35 +0600

    Fill TurnOffWateringActivity class.

--- app/src/main/java/pro/kbgame/demeter/view/TurnOffWateringActivity.java
+++ app/src/main/java/pro/kbgame/demeter/view/TurnOffWateringActivity.java
@@ -1,13 +1,78 @@
 package pro.kbgame.demeter.view;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pro.kbgame.demeter.R;
 
 public class TurnOffWateringActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvWateringFieldOne)
+    TextView tvWateringFieldOne;
+
+    @BindView(R.id.tvWateringFieldTwo)
+    TextView tvWateringFieldTwo;
+
+    @BindView(R.id.tvWateringFieldThree)
+    TextView tvWateringFieldThree;
+
+    @BindView(R.id.tvWateringFieldFour)
+    TextView tvWateringFieldFour;
+
+    @BindView(R.id.tvWateringFieldFive)
+    TextView tvWateringFieldFive;
+
+    @BindView(R.id.tvWateringFieldSix)
+    TextView tvWateringFieldSix;
+
+    @BindView(R.id.swWateringFieldOne)
+    SwitchCompat swWateringFieldOne;
+
+    @BindView(R.id.swWateringFieldTwo)
+    SwitchCompat swWateringFieldTwo;
+
+    @BindView(R.id.swWateringFieldThree)
+    SwitchCompat swWateringFieldThree;
+
+    @BindView(R.id.swWateringFieldFour)
+    SwitchCompat swWateringFieldFour;
+
+    @BindView(R.id.swWateringFieldFive)
+    SwitchCompat swWateringFieldFive;
+
+    @BindView(R.id.swWateringFieldSix)
+    SwitchCompat swWateringFieldSix;
+
+    @BindView(R.id.swFillingShowerBarrel)
+    SwitchCompat swFillingShowerBarrel;
+
+    @BindView(R.id.swFillingWateringBarrel)
+    SwitchCompat swFillingWateringBarrel;
+
+    @BindView(R.id.btCancel)
+    Button btCancel;
+
+    @BindView(R.id.btSave)
+    Button btSave;
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.btCancel)
+    public void btCancelClick() {
+        finish();
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.btSave)
+    public void btSaveClick() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +82,55 @@ public class TurnOffWateringActivity extends AppCompatActivity {
         initUI();
     }
 
-    private void initUI(){}
+    private void initUI(){
+        setSwitchesListeners();
+    }
+
+    private void setSwitchesListeners(){
+        swWateringFieldOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+        swWateringFieldTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+        swWateringFieldThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+        swWateringFieldFour.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+        swWateringFieldFive.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+        swWateringFieldSix.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            }
+        });
+
+        swFillingShowerBarrel.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        swFillingWateringBarrel.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+    }
 }
